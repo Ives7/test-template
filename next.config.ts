c069a520fef5eb259a41a9dad5a3f1f54fcef2c9
Lynch <lynch@gate.me>
2026-02-03 14:09:16 +0800

    chore: 增加项目基础设施

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    parallelServerCompiles: true,
+    parallelServerBuildTraces: true,
+    serverMinification: false,
+    webpackBuildWorker: true,
+    turbopackTreeShaking: true,
+    turbopackRemoveUnusedImports: true,
+    turbopackRemoveUnusedExports: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  typedRoutes: true,
 };
 
 export default nextConfig;
